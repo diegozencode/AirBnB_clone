cc93fcd1f308d1f15eac2ba40c41a65e4ca02a8c
Diego Monroy <difemo91@gmail.com>
2019-11-10 01:56:01 +0000

    [FEATURE] add instance from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -11,13 +11,25 @@ class BaseModel:
     Class Base Model
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Method Init Constructor
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs is not None and len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'created_at' or key == 'updated_at':
+                    setattr(
+                        self,
+                        key,
+                        datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
