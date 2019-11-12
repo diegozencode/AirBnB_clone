cfe8d7c656df6a50d67963d70751650db685c7d0
arq-gabo <jgabrielguerral@gmail.com>
2019-11-12 03:26:57 +0000

    [FEATURE]Upload_console_file

---
+++ console.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+Module Console
+"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    class command
+    """
+    intro = ''
+    prompt = '(hbnb)'
+
+    def do_quit(self, argv):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self):
+        """Method for exit the program"""
+        return True
+
+    def emptyline(self):
+        """This method do not execute anything"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
