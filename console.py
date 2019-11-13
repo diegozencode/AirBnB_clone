67d7b903eb8fac45076a522f08218b150b553c29
Diego Monroy <difemo91@gmail.com>
2019-11-13 01:33:09 +0000

    [FIX] correct output when finding the id

--- console.py
+++ console.py
@@ -6,6 +6,7 @@ Module Console
 
 import cmd
 from models.base_model import BaseModel
+import models
 
 
 class HBNBCommand(cmd.Cmd):
@@ -16,7 +17,6 @@ class HBNBCommand(cmd.Cmd):
     sp = "=======================================\n"
     intro = "{}{}type help or ? to list commands. \n".format(msg, sp)
     prompt = "(hbnb) "
-    my_classes = ["BaseModel"]
 
     def do_quit(self, args):
         """ quit command to exit the program """
@@ -58,8 +58,9 @@ class HBNBCommand(cmd.Cmd):
         print("Save it to JSON File")
         print("Usage: \n\t (hbnb) create BaseModel\n")
 
-    def show(self, args):
+    def do_show(self, args):
         """ print the string representation of an instance """
+        my_classes = ["BaseModel"]
         if args == "":
             print("** class name missing **\n")
         else:
@@ -68,7 +69,13 @@ class HBNBCommand(cmd.Cmd):
                 print("** class doesn't exist **\n")
             else:
                 if len(my_list) == 1:
-                    print("** instance id is missing **")
+                    print("** instance id is missing **\n")
+                else:
+                    myobj = "{}.{}".format(my_list[0], my_list[1])
+                    if myobj in models.storage.all().keys():
+                        print(models.storage.all()[myobj])
+                    else:
+                        print("** no instance found **\n")
 
     def do_destroy(self, args):
         """Deletesan instance based on the class name and id"""
