31e61a8b1d54b3e3a5a1e95806a7bfbc8e3f14ad
arq-gabo <jgabrielguerral@gmail.com>
2019-11-13 19:54:11 +0000

    [FEATURE]Upload_state_file

---
+++ models/state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+State Module
+"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    State class
+    """
+
+    name = ""
+
+    def __init__(self, *args, **kwarg):
+        """
+        Method for initialize State Class
+        """
+        super().__init__(*args, **kwargs)
