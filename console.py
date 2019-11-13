c70a56eb89a12af80ea69341cb1a00964ea21526
arq-gabo <jgabrielguerral@gmail.com>
2019-11-13 00:55:18 +0000

    [FIX]Incluide_destroy_method

--- console.py
+++ console.py
@@ -18,7 +18,7 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
     my_classes = ["BaseModel"]
 
-    def do_quit(self, s):
+    def do_quit(self, args):
         """ quit command to exit the program """
         return True
 
@@ -26,7 +26,7 @@ class HBNBCommand(cmd.Cmd):
         """ help quit message """
         print("Quit command to exit the program\n")
 
-    def do_EOF(self, s):
+    def do_EOF(self, args):
         """ quit program"""
         print("")
         return True
@@ -39,13 +39,13 @@ class HBNBCommand(cmd.Cmd):
         """ This method does not execute anything """
         pass
 
-    def do_create(self, s):
+    def do_create(self, args):
         """ create a new instance """
         obj = BaseModel()
-        if s == "":
+        if args == "":
             print("** class name missing **\n")
         else:
-            my_list = s.split()
+            my_list = args.split()
             if my_list[0] != obj.__class__.__name__:
                 print("** class doesn't exist **\n")
             else:
@@ -55,22 +55,42 @@ class HBNBCommand(cmd.Cmd):
     def help_create(self):
         """ help massage create """
         print("Command that saves a new register in the file storage\n")
+        print("Save it to JSON File")
+        print("Usage: \n\t (hbnb) create BaseModel\n")
 
-    def show(self, s):
+    def show(self, args):
         """ print the string representation of an instance """
-        if s == "":
+        if args == "":
             print("** class name missing **\n")
         else:
-            my_list = s.split()
-            if my_list[0] is not in my_classes:
+            my_list = args.split()
+            if my_list[0] not in my_classes:
                 print("** class doesn't exist **\n")
             else:
                 if len(my_list) == 1:
                     print("** instance id is missing **")
 
-    def help_create(self):
-        print("Save it to JSON File")
-        print("Usage: \n\t (hbnb) create BaseModel\n")
+    def do_destroy(self, args):
+        """Deletesan instance based on the class name and id"""
+        my_list = args.split()
+        if len(my_list) is 0:
+            print("** class name missing **")
+            return False
+                    
+        if my_list[0] in classes:
+            if my_list > 1:
+                key = my_list[0] + "." + my_list[1]
+                if key in my_list:
+                    del model.storage.all()[key]
+                    models.storage.save()
+                else:
+                    print("** instance id missing **")
+            else:
+                print("** instance id missing **")
+        else:
+            print("** class doesn't exist **")
+
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
